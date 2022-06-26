30908eb9fdec1bcbc42bcb0b424323219c880f59
zz-hh-aa <zzhhaa@protonmail.com>
2022-06-26 15:32:07 +0100

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; 
+    var yyyy = today.getFullYear();
+    datedd.append(dd);
+    datemm.append(mm);
+    dateyyyy.append(yyyy); 
+   
+});
